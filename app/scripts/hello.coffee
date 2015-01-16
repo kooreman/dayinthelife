1bd6fe60451df4d1483ed19831197ab9105884fb
Martijn Kooreman <m.kooreman@gmail.com>
2015-01-16 12:48:15 +0100

    init

---
+++ app/scripts/hello.coffee
@@ -0,0 +1,4 @@
+### jshint white:false ###
+
+'use strict'
+console.log '\'Hello from DOON.nu! ;)'
---
+++ app/scripts/hello.js
@@ -0,0 +1,10 @@
+
+/* jshint white:false */
+
+(function() {
+  'use strict';
+
+    $.stellar();
+    $('a').smoothScroll();
+
+}).call(this);
