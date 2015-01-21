9e89256632c2674917c40a39d10056a52557cbce
Martijn Kooreman <m.kooreman@gmail.com>
2015-01-21 15:08:46 +0100

    dist

--- scripts/031a55e8.main.js
+++
@@ -1,1 +0,0 @@
-(function(){"use strict";Modernizr.touch||$.stellar(),$("a").smoothScroll({offset:-70}),$("#contactform").submit(function(a){a.preventDefault(),$.ajax({url:"//formspree.io/info@doon.nu",method:"POST",data:{message:"hello!"},dataType:"json"}),$("#contactform_ok").fadeIn()})}).call(this);
